014b6404491248d806d6327c7c7560d067a0f63c
LatifahAlGhamdi <Latifah.Dafer.abt.js@tuwaiq.edu.sa>
2021-10-13 19:17:34 +0300

    Done HW4.

---
+++ conditional.js
@@ -0,0 +1,10 @@
+let number = 6
+if (number % 3 == 0) {
+    console.log("Fizz")
+} else if (number % 5 == 0) {
+    console.log("Buzz")
+} else if (number % 3 == 0 && number % 5 == 0) {
+    console.log("FizzBuzz")
+} else {
+    console.log(number)
+}
